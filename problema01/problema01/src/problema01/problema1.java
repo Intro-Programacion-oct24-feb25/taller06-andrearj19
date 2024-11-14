ffe1d35294c0818352f4f98082c3d45e42d0b202
unknown <aaarias19@utpl.edu.ec>
2024-11-14 12:54:08 -0500

    cambios

---
+++ problema01/problema01/src/problema01/problema1.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problema01;
+import java.util.Scanner;
+import java.util.Locale;
+/**
+ *
+ * @author utpl
+ */
+public class problema1 {
+ /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner datos = new Scanner (System.in);
+        datos.useLocale(Locale.US);
+        int valor1;
+        int valor2;
+        double suma;
+         double resta;
+          double multiplicacion;
+           double division;
+           double modulo;
+           double potencia;
+           
+           System.out.println("Ingrese el primer valor a operar");
+           valor1 = datos.nextInt();
+           
+           System.out.println("Ingrese el segundo valor a operar");
+           valor2 = datos.nextInt();
+           
+           System.out.println("Ingrese el primer valor a operar");
+           valor1 = datos.nextInt();
+           
+    }
+    
+}
+
